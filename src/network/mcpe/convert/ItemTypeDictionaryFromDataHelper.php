2398a0ec9255de1c8188bff0e23ebd61ec834a2e
Dries C <coenendries3@gmail.com>
2024-04-27 13:57:01 +0200

    Fix #462

--- src/network/mcpe/convert/ItemTypeDictionaryFromDataHelper.php
+++ src/network/mcpe/convert/ItemTypeDictionaryFromDataHelper.php
@@ -40,7 +40,7 @@ final class ItemTypeDictionaryFromDataHelper{
 
 	private const PATHS = [
 		ProtocolInfo::CURRENT_PROTOCOL => "",
-		ProtocolInfo::PROTOCOL_1_20_70 => "-1.20.60",
+		ProtocolInfo::PROTOCOL_1_20_70 => "-1.20.70",
 		ProtocolInfo::PROTOCOL_1_20_60 => "-1.20.60",
 		ProtocolInfo::PROTOCOL_1_20_50 => "-1.20.50",
 		ProtocolInfo::PROTOCOL_1_20_40 => "-1.20.40",
